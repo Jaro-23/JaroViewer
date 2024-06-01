a1b7ac8aed80256ca705c1607b4541357bdf5a94
Jaro-23 <robbejaeken23@gmail.com>
2024-06-01 20:20:59 +0200

    Tested premake

--- research/premake/premake5.lua
+++ research/premake/premake5.lua
@@ -1,2 +1,17 @@
-workspace "research"
-	configurations { "Debug", "Release" }
+workspace "HelloWorld"
+   configurations { "Debug", "Release" }
+
+project "HelloWorld"
+   kind "ConsoleApp"
+   language "C++"
+   targetdir "bin/%{cfg.buildcfg}"
+
+   files { "**.h", "**.cpp" }
+
+   filter "configurations:Debug"
+      defines { "DEBUG" }
+      symbols "On"
+
+   filter "configurations:Release"
+      defines { "NDEBUG" }
+      optimize "On"
