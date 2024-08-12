2dbeae6e82ccfdb70271828809090e7f7e2ad13a
Jaro-23 <robbejaeken23@gmail.com>
2024-08-12 12:10:04 +0200

    File structure refactor

--- research/premake/Libraries/HelloWorld/Include/test/printer.h
+++
@@ -1,8 +0,0 @@
-#include <iostream>
-
-class printer {
-public:
-	static void print() {
-		std::cout << "This is printed in the library" << std::endl;
-	}
-};
--- research/premake/Projects/HelloWorld/src/main.cpp
+++
@@ -1,7 +0,0 @@
-#include <printer.h>
-
-int main(void) {
-   std::cout << "Hello World!" << std::endl;
-   printer::print();
-   return 0;
-}
